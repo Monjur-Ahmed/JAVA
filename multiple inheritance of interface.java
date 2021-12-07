25a253d6c4a20a61ac043665f2a0ec8bc4aa34cd
Monjur-Ahmed <52495005+Monjur-Ahmed@users.noreply.github.com>
2021-12-07 20:13:45 +0600

    Create multiple inheritance of interface.java

---
+++ multiple inheritance of interface.java
@@ -0,0 +1,22 @@
+interfaceAnimalEat {
+
+void eat();
+}
+interfaceAnimalTravel {
+void travel();
+}
+class Animal implements AnimalEat, AnimalTravel {
+public void eat() {
+System.out.println("Animal is eating");
+   }
+public void travel() {
+System.out.println("Animal is travelling");
+   }
+}
+public class Demo {
+public static void main(String args[]) {
+      Animal a = new Animal();
+a.eat();
+a.travel();
+   }
+}
